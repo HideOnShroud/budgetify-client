d222113bd23ba3a9da28b96c368625ade042e25e
HideOnShroud <natroshviliguga5@gmail.com>
2024-02-16 14:08:29 +0400

    Added ChakraUI, Space Grotesk and Alata fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import '@fontsource/space-grotesk'
+
+const theme = extendTheme(
+
+  {
+    fonts: {
+      body: `'Space Grotesk', sans-serif`,
+      heading: `'Space Grotesk', sans-serif`,
+
+    },
+  }
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
