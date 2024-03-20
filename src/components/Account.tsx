27e90416c7b863846b541684b6ad14772d7a8d63
HideOnShroud <natroshviliguga5@gmail.com>
2024-03-20 12:02:28 +0400

    hovering Edit Button Work In Progress

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,7 +1,18 @@
-import { Grid, GridItem, Heading, Circle, } from "@chakra-ui/react";
+import {
+    Grid, GridItem, Heading, Circle, Button, Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure
+} from "@chakra-ui/react";
 import { AccountInterface } from "../entities/AccountInterface";
 import { getCookie, setCookie } from 'typescript-cookie'
 import { useAccount } from "../store";
+import { useState, useRef } from "react";
+
 
 interface props {
     accountInfo: AccountInterface
@@ -10,30 +21,33 @@ interface props {
 
 const Account = ({ accountInfo }: props) => {
     const setAccountId = useAccount((state) => state.setAccountId)
-
-    const commonGridProps = {
-        templateRows: 'repeat(2,0fr)',
-        templateColumns: 'repeat(5,1fr)',
-        overflow: 'hidden',
-        mb: 12,
-        p: 4,
-        borderRadius: '2xl',
-        bgGradient: 'linear(to-br, #FA777F,#E16CD0, #72AFED)',
-        textColor: "#F8F8F8",
-        maxW: 'md',
-        boxShadow: "5px 10px 20px 0px #00000040"
-    }
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [hover, setHover] = useState(Boolean)
+    const infoRef = useRef<HTMLButtonElement>(null)
 
     return (
 
         <Grid
-            {...commonGridProps}
+            templateRows='repeat(2,0fr)'
+            templateColumns='repeat(5,1fr)'
+            overflow='hidden'
+            mb='12'
+            p='4'
+            borderRadius='2xl'
+            bgGradient='linear(to-br, #FA777F,#E16CD0, #72AFED)'
+            textColor="#F8F8F8"
+            maxW='md'
+            boxShadow="5px 10px 20px 0px #00000040"
             opacity={accountInfo._id !== getCookie("accountId") ? "40%" : undefined}
             onClick={() => {
                 setCookie("accountId", accountInfo._id)
                 setAccountId(accountInfo._id)
 
             }}
+            onPointerEnter={() => {
+                setHover(true)
+            }}
+            onPointerLeave={() => { setHover(false) }}
         >
             <GridItem rowSpan={1} colSpan={3}>
                 <Heading
@@ -59,7 +73,7 @@ const Account = ({ accountInfo }: props) => {
                     {accountInfo.balance}
                 </Heading>
             </GridItem>
-            <GridItem rowSpan={2} colSpan={1} rowStart={1} colStart={5}>
+            <GridItem rowSpan={1} colSpan={1} rowStart={1} colStart={5}>
                 <Circle bg="white" padding="12px 25px 12px 25px">
                     <Heading
                         bgGradient="linear(to-br, #FA777F,#E16CD0, #72AFED)"
@@ -71,7 +85,13 @@ const Account = ({ accountInfo }: props) => {
                     </Heading>
                 </Circle>
             </GridItem>
-        </Grid>
+            {hover ? <GridItem rowSpan={1} colSpan={1} rowStart={2} colStart={5}>
+                <Grid
+                    textAlign={'center'} gap={'px'}>
+                    <GridItem><Button ref={infoRef} onClick={onOpen}>Info</Button></GridItem>
+                </Grid>
+            </GridItem > : []}
+        </Grid >
 
     )
 }
